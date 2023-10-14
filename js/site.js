221da72b128dd9e0b771672b95c57418d4d29894
Hakly Oeng <helihaung@gmail.com>
2023-10-13 20:15:10 -0400

    adjust generateFizzBuzz

--- js/site.js
+++ js/site.js
@@ -1,64 +1,74 @@
 function getValues() {
-    let fizzValues = document.getElementById('fizzValue').value;
-    let buzzValues = document.getElementById('buzzValue').value;
-    let stopValues = document.getElementById('stopValue').value;
 
+    let fizzValue = document.getElementById('fizzValue').value; 
+    let buzzValue = document.getElementById('buzzValue').value; 
+    let stopValue = document.getElementById('stopValue').value; 
 
-    fizzValues = parseInt(fizzValues);
-    buzzValues = parseInt(buzzValues);
-    stopValues = parseInt(stopValues);
-
-    if (Number.isInteger(fizzValues) && Number.isInteger(buzzValues) && Number.isInteger(stopValues) && fizzValues < 0) {
+    fizzValue = parseInt(fizzValue); 
+    buzzValue = parseInt(buzzValue); 
+    stopValue = parseInt(stopValue); 
+ 
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue) && fizzValue <= 0) {
         Swal.fire({
             icon: 'error',
-            title: 'Let keep thing in the Positive!',
-            text: 'Please enter positive number :)'
+            title: 'Let keep thing above ZERO!',
+            text: 'Please enter Number that are greater than ZERO :)'
         });
-    } else if (Number.isInteger(fizzValues) && Number.isInteger(buzzValues) && Number.isInteger(stopValues) && buzzValues < 0) {
+    } else if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue) && buzzValue <= 0) {
         Swal.fire({
             icon: 'error',
-            title: 'Let keep thing in the Positive!',
-            text: 'Please enter positive number :)'
+            title: 'Let keep thing above ZERO!',
+            text: 'Please enter Number that are greater than ZERO :)'
         });
-    } else if (Number.isInteger(fizzValues) && Number.isInteger(buzzValues) && Number.isInteger(stopValues) && stopValues < (fizzValues * buzzValues)) {
+    } else if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue) && stopValue <= 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Let keep thing above ZERO!',
+            text: 'Please enter Number that are greater than ZERO :)'
+        });
+    } else if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue) && stopValue < (fizzValue * buzzValue)) {
         Swal.fire({
             icon: 'error',
             title: 'Let give it somemore number to work with >_<',
-            text: 'Make sure the Multiplication of Fizz Values and Buzz Values is greater then Stop Value so we get atleast one FizzBuzz :)'
+            text: 'Make sure the Multiplication of FizzValue and BuzzValue is greater then StopValue so we get at least one FizzBuzz :)'
         });
-    } else if (Number.isInteger(fizzValues) && Number.isInteger(buzzValues) && Number.isInteger(stopValues) && stopValues > 9000) {
+    } else if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue) && stopValue > 9000) {
         Swal.fire({
             icon: 'error',
             title: '>_< IT OVER 9000 >_<',
-            text: 'Ensure that the Number you pick doesnt go over the LIMIT:)'
+            text: 'Ensure that the Number you pick does not go over the LIMIT:)'
         });
-    } else if (Number.isInteger(fizzValues) && Number.isInteger(buzzValues) && Number.isInteger(stopValues)) {
-        let generateNumbers = generateValues(stopValues);
-        displayValues(generateNumbers, fizzValues, buzzValues);
+    } else if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue)) {
+        let generateNumbers = generateFizzBuzz(stopValue, fizzValue, buzzValue);
+        displayFizzBuzz(generateNumbers);
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Oops!',
-            text: 'Please enter valid numbers nad ensure that your START number is less than your END number.'
+            text: 'Please enter valid numbers.'
         });
-
-    }
-
-
-}
-function generateValues(stop) {
-
-    let numbers = [];
-    for (let n = 1; n <= stop; n = n + 1) {
-        if (stop <= 9000) {
-            numbers.push(n);
-        }       
-    }
-
-    return numbers;
+    }   
 }
 
-function displayValues(numbers, fizz, buzz) {
+function generateFizzBuzz(stopValue, fizzValue, buzzValue) {
+
+    let FizzBuzz = [];
+    for (let n = 1; n <= stopValue; n = n + 1) {
+        
+        if (n % fizzValue == 0 && n % buzzValue == 0) {
+           FizzBuzz.push('FizzBuzz');
+        } else if (n % fizzValue == 0) {
+            FizzBuzz.push('Fizz');
+        } else if (n % buzzValue == 0) {
+            FizzBuzz.push('Buzz');
+        } else{
+            FizzBuzz.push(n); 
+        }          
+    }
+    return FizzBuzz;
+}
+
+function displayFizzBuzz(numbers) {
     let html = '';
 
     for (let index = 0; index < numbers.length; index += 1) {
@@ -67,20 +77,17 @@ function displayValues(numbers, fizz, buzz) {
 
         let className = '';
 
-        if (currentNumbers % fizz == 0 && currentNumbers % buzz == 0) {
-            className = 'fizzBuzz',
-            currentNumbers = 'FizzBuzz';
-        } else if (currentNumbers % fizz == 0) {
+        if (currentNumbers == 'FizzBuzz') {
+            className = 'fizzBuzz';
+        } else if (currentNumbers == 'Fizz') {
             className = 'fizz';
-            currentNumbers = 'Fizz';
-        } else if (currentNumbers % buzz == 0) {
+        } else if ( currentNumbers == 'Buzz') {
             className = 'buzz';
-            currentNumbers = 'Buzz'
-        }        
-        html += `<div class="col border border-1 ${className}">${currentNumbers}</div>`
+        }
+        html += `<div class="col border ${className}">${currentNumbers}</div>`;
     }
 
-    let col = document.getElementById('results');
+    let column = document.getElementById('results');
 
-    col.innerHTML = html;
+    column.innerHTML = html;
 }
